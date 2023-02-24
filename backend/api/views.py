5f791d65fff045544f9d36665f04de10ba2f4342
Анна Андреева <a_n_y_a_s@mail.ru>
2023-02-24 18:26:49 +0300

    fixed to pep8

--- backend/api/views.py
+++ backend/api/views.py
@@ -7,16 +7,15 @@ from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 
-from recipes.models import (Favorite, Ingredient, IngredientAmount, Recipe,
-                            ShoppingCart, Tag)
-from users.models import Subscription, User
-
 from .filters import RecipeFilter
 from .permissions import IsAuthorOrAdminOrReadOnly
 from .serializers import (IngredientSerializer, RecipeReadSerializer,
                           RecipeSerializer, RecipeWriteSerializer,
                           SubscribeSerializer, SubscriptionSerializer,
                           TagSerializer)
+from recipes.models import (Favorite, Ingredient, IngredientAmount, Recipe,
+                            ShoppingCart, Tag)
+from users.models import Subscription, User
 
 
 def post_delete_favorite_shopping_cart(request, model, id):
