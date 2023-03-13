ed44c8ae6b435aa47083f08455160eb164659ad5
Анна Андреева <a_n_y_a_s@mail.ru>
2023-03-13 12:55:07 +0300

    admin

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Favorite, Ingredient, IngredientAmount, Recipe, Tag
+from .models import Ingredient, IngredientAmount, Recipe, Tag
 
 
 class IngredientsInline(admin.TabularInline):
